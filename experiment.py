9e94b87accf2ae898f159cb2355e5d15da6dace1
nishimura5 <super.nishimura@gmail.com>
2023-06-20 15:47:12 +0900

    撮影条件を変更できるように修正

--- experiment.py
+++ experiment.py
@@ -8,7 +8,13 @@ if __name__ == "__main__":
     que_game_to_cv2 = Queue()
     que_cv2_to_game = Queue()
 
-    loop_thread = threading.Thread(target=gopro_ctrl.loop, args=(que_game_to_cv2, que_cv2_to_game, 60))
+    ## 撮影条件
+    horizon_mode = True
+    duration_sec = 60
+
+    loop_thread = threading.Thread(
+        target=gopro_ctrl.loop,
+        args=(que_game_to_cv2, que_cv2_to_game, duration_sec, horizon_mode))
     loop_thread.start()
 
     game = Game()
